77ff4aa8abe58de154f3bc974901eb630885ecc8
Abdallah Moussawi <abdallah.moussawi@gmail.com>
2018-03-31 02:24:46 +0300

    refactor RecommendationDataset

--- recoder/data.py
+++ recoder/data.py
@@ -5,48 +5,22 @@ import glog as log
 
 class RecommendationDataset(Dataset):
 
-  def __init__(self, data_file, item_based=True, target_dataset=None,
-               user_dtype=None, item_dtype=None, inter_dtype=np.int32,
-               user_col='user', item_col='item', inter_col='inter',
-               data_apply_fn=None):
+  def __init__(self, data=None, target_dataset=None,
+               user_col='user', item_col='item', inter_col='inter'):
 
-    self.data_file = data_file
-
-    self.item_based = item_based
-
-    self.user_dtype = user_dtype
-    self.item_dtype = item_dtype
-    self.inter_dtype = inter_dtype
-    self.data_apply_fn = data_apply_fn
+    self.data = data
 
     self.user_col = user_col
     self.item_col = item_col
     self.inter_col = inter_col
     self.target_dataset = target_dataset # type: RecommendationDataset
 
-    self.index_col = self.user_col if self.item_based else self.item_col
-    self.target_col = self.item_col if self.item_based else self.user_col
+    self.index_col = self.user_col
+    self.target_col = self.item_col
 
     self.__load_data()
 
   def __load_data(self):
-    log.info('Loading dataset from: ' + str(self.data_file))
-
-    _pd_dtype = {
-      self.inter_col: self.inter_dtype,
-    }
-
-    if self.user_dtype is not None:
-      _pd_dtype[self.user_col] = self.user_dtype
-
-    if self.item_dtype is not None:
-      _pd_dtype[self.item_col] = self.item_dtype
-
-    self.data = pd.read_csv(self.data_file, dtype=_pd_dtype)
-
-    if self.data_apply_fn is not None:
-      self.data = self.data_apply_fn(self.data)
-
     self.users = self.data[self.user_col].unique().tolist()
     self.items = self.data[self.item_col].unique().tolist()
 
