fd095ae371109e299425b9a44c92e273099d19ab
Abdallah Moussawi <abdallah.moussawi@gmail.com>
2018-04-07 13:02:59 +0300

    process similarity recommendations in batches

--- recoder/recommender.py
+++ recoder/recommender.py
@@ -1,7 +1,11 @@
 from recoder.embedding import EmbeddingsIndex
-import heapq
 
 import numpy as np
+
+import recoder.utils as utils
+
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 class Recommender(object):
 
@@ -11,44 +15,54 @@ class Recommender(object):
 class SimilarityRecommender(Recommender):
 
   def __init__(self, embeddings_index: EmbeddingsIndex,
-               num_recommendations, scale=1, search_k=10000,
-               pool_size=-1):
+               num_recommendations, n=1, scale=1):
     self.embeddings_index = embeddings_index
     self.scale = scale
     self.num_recommendations = num_recommendations
-    self.search_k = search_k
-    self.pool_size = pool_size if pool_size > 0 else self.num_recommendations
+    self.n = n
 
-  def recommend(self, user_hist):
-    user_items = list(zip(*user_hist))[0]
+  def __recommend_single(self, user_hist):
+    user_items = np.array(utils.unzip(user_hist)[0])
 
-    num_s = int(self.pool_size / len(user_items))
+    items_pool = [self.embeddings_index.get_nns_by_id(item_id, self.n)
+                  for item_id in user_items]
 
-    items_pool = []
-    for item_id in user_items:
-      if item_id in self.embeddings_index.id_map:
-        items_pool += self.embeddings_index.get_nns_by_id(item_id, num_s, search_k=self.search_k)
+    items_pool = np.unique(items_pool)
+    filtered_items = items_pool[np.isin(items_pool, user_items, invert=True)]
 
-    items_pool = set(items_pool)
+    items_scores = self.__compute_scores(filtered_items, user_items)
 
-    recommendations_heap = []
-    for item_id in items_pool:
-      if item_id in user_items: continue
-      item_score = 0
-      for user_item in user_items:
-        if user_item in self.embeddings_index.id_map:
-          item_score += self.embeddings_index.get_similarity(item_id, user_item) ** self.scale
+    if len(items_scores) > self.num_recommendations:
+      top_ind_not_sorted = np.argpartition(-items_scores, self.num_recommendations)
+      top_ind_not_sorted = top_ind_not_sorted[:self.num_recommendations]
+    else:
+      top_ind_not_sorted = np.arange(len(items_scores))
 
-      if len(recommendations_heap) < self.num_recommendations:
-        heapq.heappush(recommendations_heap, (item_score, item_id))
-      elif recommendations_heap[0][0] < item_score:
-        heapq.heapreplace(recommendations_heap, (item_score, item_id))
+    top_sorted_reset_ind = np.argsort(-items_scores[top_ind_not_sorted])
 
-    recommendations = []
-    while recommendations_heap:
-      recommendations.append(heapq.heappop(recommendations_heap)[1])
-    recommendations.reverse()
+    top_ind_sorted = top_ind_not_sorted[top_sorted_reset_ind]
+    top_items = filtered_items[top_ind_sorted]
 
+    return top_items
+
+  def __compute_scores(self, items_pool, user_items):
+    pool_embeddings = np.array([self.embeddings_index.get_embedding(item_id)
+                                for item_id in items_pool])
+    user_embeddings = np.array([self.embeddings_index.get_embedding(item_id)
+                                for item_id in user_items])
+
+    scores = cosine_similarity(pool_embeddings, user_embeddings) # range: -1 to 1
+    scores = (scores + 1) / 2 # range: 0 to 1
+
+    scaled_scores = np.power(scores, self.scale)
+
+    agg_scores = np.sum(scaled_scores, axis=1)
+
+    return agg_scores
+
+  def recommend(self, users_hist):
+    recommendations = [self.__recommend_single(user_hist)
+                       for user_hist in users_hist]
     return recommendations
 
 
