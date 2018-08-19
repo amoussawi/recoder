95dbb58111657bfe05a30eda2af1aff86da89005
Abdallah Moussawi <abdallah.moussawi@gmail.com>
2018-08-20 00:20:50 +0300

    implement vector/matrix normalization function utility

--- recoder/utils.py
+++ recoder/utils.py
@@ -1,5 +1,17 @@
+import numpy as np
 
 
 def unzip(l):
   a, b = list(zip(*l))
   return list(a), list(b)
+
+
+def normalize(x, axis=None):
+  """
+  Returns the normalization of `x` along `axis`.
+
+  Args:
+    x (np.array): matrix or vector
+    axis (int, optional): the axis along which to compute the normalization
+  """
+  return x / np.linalg.norm(x, axis=axis).reshape(-1, 1)
