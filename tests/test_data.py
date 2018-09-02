6abcb2672c3e3a8c415a846cd37f366d1632953b
Abdallah Moussawi <abdallah.moussawi@gmail.com>
2018-09-02 18:49:45 +0300

    update RecommendationDataset tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -13,7 +13,7 @@ def input_dataframe():
   data['user'] = np.random.randint(1, 50, 300)
   data['item'] = np.random.randint(1, 200, 300)
   data['inter'] = np.ones(300)
-
+  data = data.drop_duplicates(['user', 'item'])
   return data
 
 
@@ -23,31 +23,48 @@ def target_dataframe():
   data['user'] = np.random.randint(1, 50, 300)
   data['item'] = np.random.randint(1, 200, 300)
   data['inter'] = np.ones(300)
-
+  data = data.drop_duplicates(['user', 'item'])
   return data
 
 
-def test_RecommendationDataset(input_dataframe):
-  dataset = RecommendationDataset(data=input_dataframe)
+@pytest.mark.parametrize("num_workers",
+                         [0, 4])
+def test_RecommendationDataset(input_dataframe, num_workers):
+  dataset = RecommendationDataset()
+  dataset.fill_from_dataframe(input_dataframe, num_workers=num_workers)
 
   assert len(dataset) == len(np.unique(input_dataframe['user']))
 
-  test_index = np.random.randint(0, len(dataset))
+  replica_df = pd.DataFrame(input_dataframe)
 
-  input_interactions, target_interactions = dataset[test_index]
+  for index in range(len(dataset)):
+    user = dataset.users[index]
+    interactions = dataset[index]
 
-  assert len(input_interactions) > 0 and len(target_interactions) > 0
+    assert len(interactions) == len(replica_df[replica_df.user == user])
 
-  assert input_interactions == target_interactions
+    for item_id, inter_val in interactions:
+      assert len(replica_df[(replica_df.user == user)
+                            & (replica_df.item == item_id)
+                            & (replica_df.inter == inter_val)]) > 0
+      replica_df = replica_df[~ ((replica_df.user == user)
+                                & (replica_df.item == item_id)
+                                & (replica_df.inter == inter_val))]
+
+    assert not type(interactions) is tuple
+    assert len(interactions) > 0
+
+  # check that both the returned list of interactions and the dataframe contain
+  # the same of interactions
+  assert len(replica_df) == 0
 
 
 def test_RecommendationDataset_target(input_dataframe, target_dataframe):
-  target_dataset = RecommendationDataset(data=target_dataframe)
-  dataset = RecommendationDataset(data=input_dataframe,
-                                  target_dataset=target_dataset)
+  target_dataset = RecommendationDataset()
+  dataset = RecommendationDataset(target_dataset=target_dataset)
 
-  assert len(dataset) == len(np.intersect1d(np.unique(input_dataframe['user']),
-                                            np.unique(target_dataframe['user'])))
+  dataset.fill_from_dataframe(target_dataframe)
+  target_dataset.fill_from_dataframe(input_dataframe)
 
   test_index = np.random.randint(0, len(dataset))
 
@@ -56,7 +73,3 @@ def test_RecommendationDataset_target(input_dataframe, target_dataframe):
   assert len(input_interactions) > 0 and len(target_interactions) > 0
 
   assert input_interactions != target_interactions
-
-
-
-
