cd59308f44e828a88cce6db2258cd50babd702e3
Abdallah Moussawi <abdallah.moussawi@gmail.com>
2021-04-11 00:32:34 +0100

    Update dependencies and support python 3.8

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ import recoder
 setup(
   name='recsys-recoder',
   version=recoder.__version__,
-  install_requires=['torch>=0.4.1', 'annoy',
-                    'numpy', 'scipy>=1.2.0',
-                    'tqdm', 'glog'],
+  install_requires=['torch==1.8.1', 'annoy==1.17.0',
+                    'numpy==1.19.5', 'scipy==1.6.2',
+                    'tqdm==4.59.0', 'glog==0.3.1'],
   packages=find_packages(),
   author='Abdallah Moussawi',
   author_email='abdallah.moussawi@gmail.com',
